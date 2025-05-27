6c622ec0625b41a8edd61b2b59dde305bc438c67
Roshan-Silva <roshan0122silva@gmail.com>
2025-05-27 23:27:55 +0530

    display the slider data in the home page

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -5,44 +5,10 @@
 <!-- Slider Area -->
 		<section class="slider">
 			<div class="hero-slider">
-				<!-- Start Single Slider -->
-				<div class="single-slider" style="background-image:url('frontend/img/slider2.jpg')">
-					<div class="container">
-						<div class="row">
-							<div class="col-lg-7">
-								<div class="text">
-									<h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl pellentesque, faucibus libero eu, gravida quam. </p>
-									<div class="button">
-										<a href="#" class="btn">Get Appointment</a>
-										<a href="#" class="btn primary">Learn More</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<!-- End Single Slider -->
-				<!-- Start Single Slider -->
-				<div class="single-slider" style="background-image:url('frontend/img/slider.jpg')">
-					<div class="container">
-						<div class="row">
-							<div class="col-lg-7">
-								<div class="text">
-									<h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
-									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl pellentesque, faucibus libero eu, gravida quam. </p>
-									<div class="button">
-										<a href="/appointment" class="btn">Get Appointment</a>
-										<a href="#" class="btn primary">About Us</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+				@include('frontend.home.slider')
 				<!-- Start End Slider -->
 				<!-- Start Single Slider -->
-				<div class="single-slider" style="background-image:url('frontend/img/slider3.jpg')">
+				{{-- <div class="single-slider" style="background-image:url('frontend/img/slider3.jpg')">
 					<div class="container">
 						<div class="row">
 							<div class="col-lg-7">
@@ -57,7 +23,7 @@
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> --}}
 				<!-- End Single Slider -->
 			</div>
 		</section>
---
+++ resources/views/frontend/home/slider.blade.php
@@ -0,0 +1,63 @@
+@foreach ($sliders as $slider)
+    <!-- Start Single Slider -->
+    {{-- <div class="single-slider" style="background-image:url('{{ asset('storage/' . $slider->image) }}')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <div class="text">
+                        <h1>{{ $slider->title }}</h1>
+                        <p>{{ $slider->description }}</p>
+                        <div class="button">
+                            @if ($slider->button_text)
+                                <a href="{{ $slider->button_link }}" class="btn">{{ $slider->button_text }}</a>
+                            @endif
+                            @if ($slider->secondary_button_text)
+                                <a href="{{ $slider->secondary_button_link }}" class="btn primary">{{ $slider->secondary_button_text }}</a>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+
+
+    <!-- Start Single Slider -->
+<div class="single-slider" style="background-image:url({{asset('storage/'.$slider->image_link)}})">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7">
+                <div class="text">
+                    <h1>{{$slider->heading}}</h1>
+                    <p>{{ $slider->sub_heading }}</p>
+                    <div class="button">
+                        <a href="{{ $slider->get_appointment_link }}" class="btn">Get Appointment</a>
+                        <a href="{{ $slider->contact_link }}" class="btn primary">Learn More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Single Slider -->
+<!-- Start Single Slider -->
+{{-- <div class="single-slider" style="background-image:url('frontend/img/slider.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7">
+                <div class="text">
+                    <h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl pellentesque, faucibus libero eu, gravida quam. </p>
+                    <div class="button">
+                        <a href="/appointment" class="btn">Get Appointment</a>
+                        <a href="#" class="btn primary">About Us</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> --}}
+    
+@endforeach
+
+
